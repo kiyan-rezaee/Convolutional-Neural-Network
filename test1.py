ba09c0520d111574791c3930f0d98ea4b75922cb
kiyan rezaee <kiyanrezaee176@gmail.com>
2022-08-18 09:05:28 +0430

    Week 3

--- test1.py
+++ test1.py
@@ -37,14 +37,15 @@ import matplotlib.pyplot as plt
 # axarr[1].imshow(x_pad[0, :, :, 0])
 
 
-output = [['InputLayer', [(None, 64, 64, 3)], 0],
-        ['Conv2D', (None, 64, 64, 8), 392, 'same', 'linear', 'GlorotUniform'],
-        ['ReLU', (None, 64, 64, 8), 0],
-        ['MaxPooling2D', (None, 8, 8, 8), 0, (8, 8), (8, 8), 'same'],
-        ['Conv2D', (None, 8, 8, 16), 528, 'same', 'linear', 'GlorotUniform'],
-        ['ReLU', (None, 8, 8, 16), 0],
-        ['MaxPooling2D', (None, 2, 2, 16), 0, (4, 4), (4, 4), 'same'],
-        ['Flatten', (None, 64), 0],
-        ['Dense', (None, 6), 390, 'softmax']]
+# output = [['InputLayer', [(None, 64, 64, 3)], 0],
+#         ['Conv2D', (None, 64, 64, 8), 392, 'same', 'linear', 'GlorotUniform'],
+#         ['ReLU', (None, 64, 64, 8), 0],
+#         ['MaxPooling2D', (None, 8, 8, 8), 0, (8, 8), (8, 8), 'same'],
+#         ['Conv2D', (None, 8, 8, 16), 528, 'same', 'linear', 'GlorotUniform'],
+#         ['ReLU', (None, 8, 8, 16), 0],
+#         ['MaxPooling2D', (None, 2, 2, 16), 0, (4, 4), (4, 4), 'same'],
+#         ['Flatten', (None, 64), 0],
+#         ['Dense', (None, 6), 390, 'softmax']]
+
 
 
